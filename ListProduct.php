6fc3a32b3f03565688942ff97dbb0578b93912e9
ikram1 <ikramkalsel@gmail.com>
2023-11-30 17:16:44 +0700

    Update

---
+++ ListProduct.php
@@ -0,0 +1,57 @@
+<?php
+
+include './conn.php';
+
+$sql = "SELECT product.id,
+    product.name,
+    product.category_id,
+    product.supplier_id,
+    product.stok,
+    product.price,
+    product.description,
+    category.name as category,
+    supplier.name as supplier,
+    supplier.address as supplier_address,
+    supplier.phone_number as supplier_phone
+FROM product
+INNER JOIN category on product.category_id = category.category.id
+INNER JOIN supplier on product.supplier_id = supplier.id
+WHERE product.category_id = category.category.id
+AND product.supplier_id = supplier.id";
+$query = $conn->query($sql);
+
+if ($query->num_rows < 1) {
+    echo json_encode(
+        array(
+            'response' => false,
+            'message' => 'Data Not Found',
+            'payload' => null
+        )
+    );
+} else {
+    $result = [];
+    while ($row = mysqli_fetch_array($query)) {
+        array_push(
+            $result,
+            array(
+                "id" => $row["id"],
+                "category_id" => $row["category_id"],
+                "supplier_id" => $row["supplier_id"],
+                "name" => $row["name"],
+                "stok" => $row["stok"],
+                "price" => $row["price"],
+                "description" => $row["description"],
+                "category" => $row["category"],
+                "supplier" => $row["supplier"],
+                "supplier_address" => $row["supplier_address"],
+                "supplier_phone" => $row["supplier_phone"],
+            )
+        );
+    }
+    echo json_encode(array('Products' => $result));
+}
+
+// mengatur tampilan json
+
+header('Content-Type: application/json')
+?>
